d0c4abc2113e1efe530ba2d939071b6e3b9e7294
Tomoki Sakamoto <s3821551@student.rmit.edu.au>
2025-08-08 22:26:22 +1000

    Initial commit - Vue project with JSON.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import JSONComp from './components/JSON.vue'
+</script>
+
+
+<template>
+  <h1>Week 2</h1>
+  <main>
+    <JSONComp/>
+  </main>
+</template>
+
+<style scoped></style>
