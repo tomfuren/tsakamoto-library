7c83469263e09a1b0464e28709e17274608700bd
Tomoki Sakamoto <s3821551@student.rmit.edu.au>
2025-08-16 01:18:23 +1000

    Add Form.vue with card display feature

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
-<script setup>
-  import JSONComp from './components/JSON.vue'
+<script setup >
+// import JSON from "./components/JSON.vue"
+// import JSONSolution from "./components/JSONSolution.vue" 
+import Form from "./components/Form.vue";
 </script>
 
-
 <template>
-  <h1>Week 2</h1>
-  <main>
-    <JSONComp/>
-  </main>
+  <!-- <JSON />
+  <JSONSolution /> -->
+  <Form />
 </template>
 
 <style scoped></style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//import boostrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
+
 createApp(App).mount('#app')
