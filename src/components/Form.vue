4efc517b1affb59c97105d80614b77aab311f07a
Tomoki Sakamoto <s3821551@student.rmit.edu.au>
2025-08-16 15:18:00 +1000

    Week4 Activity 1&2

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,11 +7,11 @@
                     <div class="row g-3 mb-3">
                         <div class="col-6">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" v-model="formData.username">
+                            <input type="text" class="form-control" id="username" required v-model="formData.username">
                         </div>
                         <div class="col-6">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" v-model="formData.password">
+                            <input type="password" class="form-control" id="password" minlength="4" maxlength="10" v-model="formData.password">
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -19,13 +19,14 @@
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input"
                                 id="isAustralian"
-                                    v-model="formData.isAustralian">
+                                    v-model="formData.isAustralian" required>
                                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
                             </div>
                         </div>
                         <div class="col-6">
                             <label for="gender" class="form-label">Gender</label>
-                            <select class="form-select" id="gender" v-model="formData.gender">
+                            <select class="form-select" id="gender" required v-model="formData.gender">
+                                <option value="" disabled>Select...</option>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                                 <option value="other">Other</option>
@@ -34,7 +35,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="reason" class="form-label">Reason for joining</label>
-                        <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+                        <textarea class="form-control" id="reason" rows="3" v-model="formData.reason" required minlength="10" maxlength="200"></textarea>
                     </div>
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary me-2">Submit</button>
